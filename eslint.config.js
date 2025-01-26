e7e21b9787f44af6823293a14582806684ca7dc8
BarakJB <97511143+BarakJacob7@users.noreply.github.com>
2025-01-26 22:21:52 +0200

    Fix: Update eslint configuration for flat config compatibility1

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
 import pkg from 'eslint';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const { FlatConfig } = pkg;
+
+// This will give you the directory name of the current file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = new FlatConfig({
   files: ['{src,apps,libs,test}/**/*.ts'],
@@ -7,7 +14,7 @@ const config = new FlatConfig({
     parser: '@typescript-eslint/parser',  // Set the parser here
     parserOptions: {
       project: 'tsconfig.json',
-      tsconfigRootDir: __dirname,
+      tsconfigRootDir: __dirname,  // Use __dirname as the root directory
       sourceType: 'module',
     },
   },
